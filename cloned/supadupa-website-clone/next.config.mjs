cdacde3c0cb8c57c310b8e50f6308b7412a96819
beyg1 <abdulrubbaig1@gmail.com>
2025-11-06 02:40:07 +0500

    Supadupa console error rectified for i18n.ts

--- cloned/supadupa-website-clone/next.config.mjs
+++ cloned/supadupa-website-clone/next.config.mjs
@@ -2,7 +2,7 @@
 
 import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 const nextConfig = {};
 
 export default withNextIntl(nextConfig);
