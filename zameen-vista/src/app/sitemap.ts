1bbdbda5dca48f257d45d5cf8f5036467c8bcad4
beyg1 <abdulrubbaig1@gmail.com>
2025-11-14 23:55:36 +0500

    domain name updated in robots and sitemap

--- zameen-vista/src/app/sitemap.ts
+++ zameen-vista/src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://zameen-vista.vercel.app'
+  const baseUrl = 'https://landing-pages-blue.vercel.app/'
 
   return [
     {
